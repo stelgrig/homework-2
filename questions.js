a0f0e99afea5bbf237cdaf4c74b62f07e152f36a
stelgrig <stella.grigoryan.2004@gmail.com>
2020-03-23 07:25:18 -0700

    Questions

---
+++ questions.js
@@ -0,0 +1,74 @@
+let a = +prompt("greq a tiv");
+let b = +prompt("greq b tiv");                 //xndir1
+alert(a+b);
+
+
+let a = +prompt('greq a tiv');
+let b = +prompt('greq b tiv');
+ for(let i = a; i < b; I++){                    //xndir2
+    if(i % 2 !== 0){
+        console.log(1)
+    }
+ }
+
+
+ let a = +prompt('greq a tiv');
+ let b = +prompt('greq b tiv');
+  for(let i = a; i < b; I++){                    // (naxord tnayini 4)
+     if(i % 2 === 0){
+         console.log(zuyg tiv)
+     }
+  }
+
+ let a = +prompt(greq arajin tiv);
+ let b = +prompt(greq erkrord tiv);
+ let c = +prompt(greq errord tiv );
+ if(a > b && a > C){                               //xndir3
+     alert(a)
+ }
+     else if(b > a && b > C){
+         alert(b)        
+     }
+     else if(c > a && C > b){
+         alert(c)
+     }
+
+let a = +prompt(greq arajin tiv);
+let b = +prompt(greq erkrord tiv);
+let c = +prompt(greq errord tiv);
+if(a > b && a > c && b > c){
+    document.write(a,b,c);
+} else if(b > a && b > c && a > c){           //xndir4
+    document.write(b,a,c);
+} else if(c > b && c > a && b > a){
+    document.write(c,b,a);
+} else if(c > a && c > b && a > b){
+    document.write(c,a,b);
+}
+
+
+ let num = prompt(greq tiv);
+ let sum = 1;
+  while(num > 0){
+   sum *= num;                   //xndir5
+   num-- ;
+    }
+    console.log(sum)
+       
+ let a = +prompt('greq tiv')
+ let b = 0    
+ let c = 1
+  if(a==0){
+      console.log(b)
+  } else if(a==1){
+      console.log(b)                                             //xndir6
+      console.log(c)
+      }   else{
+        console.log(b)
+        console.log(c)
+        for(i = 0; i < a; i++){
+            let d =b + c
+            b = c 
+            c = d console.log(d)
+        }
+      }
